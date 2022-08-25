bda18bdfbfe131cfcb11c89ea77121965f70bd16
Teo Voinea <tevoinea@microsoft.com>
2022-08-25 12:04:34 +0000

    checkpoint

--- .devcontainer/post-create-script.sh
+++ .devcontainer/post-create-script.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Install Azure Functions Core Tools 4
-# Source: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v4%2Clinux%2Ccsharp%2Cportal%2Cbash
-echo "Installing Azure Functions Core Tools 4 ..."
-# note that 'dotnet' feature in devcontainer.json already sets up the PPA for this
-sudo apt install azure-functions-core-tools-4
-
-# Install azcopy
-echo "Installing azcopy ..."
-cd /tmp
-wget https://aka.ms/downloadazcopy-v10-linux
-tar -xvf downloadazcopy-v10-linux
-sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
-
-# Install Azurite
-sudo npm install -g azurite
-
 # Restore rust dependencies
 echo "Restoring rust dependencies"
 cargo install cargo-audit cargo-license@0.4.2 # requirements if you want to run ci/agent.sh
@@ -27,9 +11,6 @@ echo "Restore dotnet dependencies"
 cd /workspaces/onefuzz/src/ApiService
 dotnet restore
 
-sudo apt-get install direnv uuid-runtime
-pip install wheel
-
 echo "Setting up venv"
 cd /workspaces/onefuzz/src
 python -m venv venv
@@ -39,25 +20,29 @@ echo "Installing pytypes"
 cd /workspaces/onefuzz/src/pytypes
 echo "layout python3" >> .envrc
 direnv allow
-pip install -e .
+python -m pip install -e .
 
 echo "Installing cli"
 cd /workspaces/onefuzz/src/cli
 echo "layout python3" >> .envrc
 direnv allow
-pip install -e .
+pythom -m pip install -e .
 
 echo "Install api-service"
 cd /workspaces/onefuzz/src/api-service
 echo "layout python3" >> .envrc
 direnv allow
-pip install -r requirements-dev.txt
+python -m pip install -r requirements-dev.txt
 cd __app__
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 cd /workspaces/onefuzz/src/utils
 chmod u+x lint.sh
-pip install types-six
+python -m pip install types-six
 
 cp /workspaces/onefuzz/.devcontainer/pre-commit /workspaces/onefuzz/.git/hooks
 chmod u+x /workspaces/onefuzz/.git/hooks/pre-commit
+
+# TODO
+# WARNING: The script normalizer is installed in '/home/vscode/.local/bin' which is not on PATH.
+#   Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
